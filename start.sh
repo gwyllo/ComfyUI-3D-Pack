75c3c00b57c64d91bcd0d163393b3609dee85c3b
Gwyllim Jahn <gwyllo@gmail.com>
2024-08-11 13:11:18 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cloudflared tunnel --no-autoupdate run --url http://localhost:8188 &
-python main.py --listen 0.0.0.0
+cloudflared tunnel --no-autoupdate --url http://localhost:8188 &
+python /app/main.py --listen 0.0.0.0
